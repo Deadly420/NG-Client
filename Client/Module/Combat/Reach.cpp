e48d2d2d28b951bc1448c18aa32c6324d3684cb8
User0420 <88569435+DeadtrosGaming@users.noreply.github.com>
2023-09-18 17:39:32 -0400

    Reach Fixed

--- Client/Module/Combat/Reach.cpp
+++ Client/Module/Combat/Reach.cpp
@@ -28,7 +28,7 @@ void Reach::onTick(GameMode* gm) {
 void Reach::onEnable() {
 	static uintptr_t sigOffset = 0x0;
 	if (sigOffset == 0x0) {
-		sigOffset = FindSignature("48 83 ec ? e8 ? ? ? ? 48 83 c4 ? e9 ? ? ? ? cc cc cc cc cc cc cc cc cc cc cc cc cc cc e9");
+		sigOffset = FindSignature("f3 0f 10 15 ? ? ? ? 48 8d 0d ? ? ? ? 48 89 4c 24 ? 48 8d 54 24");
 
 		if (sigOffset != 0x0) {
 			int offset = *reinterpret_cast<int*>((sigOffset + 4));  // Get Offset from code
